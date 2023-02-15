47f69f668ae36117aad4e1543958eb51d30fa335
KombinatorVit <slonik0775@gmail.com>
2023-02-15 11:55:40 +0200

    cartIcon and HeaderCardButton

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./HeaderCartButton.module.css";
+
+
+const HeaderCartButton = () => {
+    return (
+        <button>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    );
+};
+
+export default HeaderCartButton;
