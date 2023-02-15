120ae2da2c768c79379ae6a63edf501e8ef47887
KombinatorVit <slonik0775@gmail.com>
2023-02-15 13:05:48 +0200

    Добавил форму + допилил стили

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from './MealItemForm.module.css'
+import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
     return (
-        <div>
-            
-        </div>
+        <form className={styles.form}>
+            <Input label={'Кол-во'} input={{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                step: '1',
+                defaultValue: '1'
+
+            }}/>
+            <button>Добавить</button>
+        </form>
     );
 };
 
