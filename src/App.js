1f5bb82b6fbfcb5c5e15a9aa8148256e010912f3
KombinatorVit <slonik0775@gmail.com>
2023-02-15 21:18:53 +0200

    context CartCustomProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import {useState} from "react";
+import CartContextProvider from "./store/CartContextProvider";
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
     }
 
     return (
-        <>
+        <CartContextProvider>
             {cartIsVisible && <Cart onHideCart={hideCartHandler}/>}
             <Header onShowCart={showCartHandler}/>
             <main>
                 <Meals/>
             </main>
-        </>
+        </CartContextProvider>
     );
 }
 
