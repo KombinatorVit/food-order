c8492007c6de5066b8f7f53292e189f5a59dffcd
KombinatorVit <slonik0775@gmail.com>
2023-02-16 15:00:50 +0200

    добавил анимацию при добавление товара в корзину, useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
+import {cleanup} from "@testing-library/react";
 
 
 const HeaderCartButton = (props) => {
+    const [buttonIsAnimated, setButtonIsAnimated] = useState(false)
     const cartContext = useContext(CartContext)
 
     const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
         return currentValue + item.amount
     }, 0)
+
+    const buttonClasses = `${styles.button} ${buttonIsAnimated ? styles.bump : ''}`
+
+
+    useEffect(() => {
+        if (cartContext.items === 0) return
+
+        setButtonIsAnimated(true)
+
+        const timer = setTimeout(() => {
+            setButtonIsAnimated(false)
+
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [cartContext.items])
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={buttonClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon/>
             </span>
